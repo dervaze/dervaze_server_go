d9edc1700aac2b188c203ab3d2d88e6718ed2906
Emre Şahin <iesahin@democritus.teknokrat.com.tr>
2020-11-24 11:16:26 +0300

    some packaging operations

--- dervaze/root.go
+++ dervaze/root.go
@@ -1,21 +1,22 @@
-package root
+package dervaze
 
 import (
-	"dervazepb"
+	"deneme/dervaze/dervaze"
 	"regexp"
 	"strings"
 )
 
+// VOWELS are the all Roman vowels recognized
 const VOWELS = "aâeıioöuûü"
 
-var ultimateVowelRegex regexp.Regexp = regexp.MustCompile(`.*([aâeıiîoöuüû])[^aâeıiîoöuüû]*$`)
-var ultimateConsonantRegex regexp.Regexp = regexp.MustCompile(`.*([^aâeıiîoöuüû])[aâeıiîoöuüû]*$`)
-var vowelsRegex regexp.Regexp = regexp.MustCompile(`([aâeıiîoöuüû])`)
-var consonantsRegex regexp.Regexp = regexp.MustCompile(`([bcçdfgğhjklmnpqrsştvwyxz])`)
-var endsWithVowelRegex regexp.Regexp = regexp.MustCompile(`.*[aeıioöuüâûî][']?$`)
-var hasSingleVowelRegex regexp.Regexp = regexp.MustCompile(`^[^aâeıiîoöuüû]*[aâeıiîoöuüû][^aâeıiîoöuüû]*$`)
-var lastConsonantHardRegex regexp.Regexp = regexp.MustCompile(`[fstkçşhp]'?$`)
-var effectiveLastVowelRegexes = map[regexp.Regexp]string{
+var ultimateVowelRegex *regexp.Regexp = regexp.MustCompile(`.*([aâeıiîoöuüû])[^aâeıiîoöuüû]*$`)
+var ultimateConsonantRegex *regexp.Regexp = regexp.MustCompile(`.*([^aâeıiîoöuüû])[aâeıiîoöuüû]*$`)
+var vowelsRegex *regexp.Regexp = regexp.MustCompile(`([aâeıiîoöuüû])`)
+var consonantsRegex *regexp.Regexp = regexp.MustCompile(`([bcçdfgğhjklmnpqrsştvwyxz])`)
+var endsWithVowelRegex *regexp.Regexp = regexp.MustCompile(`.*[aeıioöuüâûî][']?$`)
+var hasSingleVowelRegex *regexp.Regexp = regexp.MustCompile(`^[^aâeıiîoöuüû]*[aâeıiîoöuüû][^aâeıiîoöuüû]*$`)
+var lastConsonantHardRegex *regexp.Regexp = regexp.MustCompile(`[fstkçşhp]'?$`)
+var effectiveLastVowelRegexes = map[*regexp.Regexp]string{
 	regexp.MustCompile(`.*a[^aeıioöuüâûî]*$`):     `a`,
 	regexp.MustCompile(`.*â[lk][^aeıioöuüâûî]*$`): `i`,
 	regexp.MustCompile(`.*â[^lkaeıioöuüâûî]*$`):   `a`,
@@ -31,9 +32,10 @@ var effectiveLastVowelRegexes = map[regexp.Regexp]string{
 	regexp.MustCompile(`.*û[^lkaeıioöuüâûî]*$`):   `u`,
 }
 
-func MakeRoot(latin string, visenc string, pos dervazepb.PartOfSpeech) dervazepb.Root {
+// MakeRoot builds a Root from Latin and Visenc spelling of a word by automatically filling other information
+func MakeRoot(latin string, visenc string, pos dervaze.PartOfSpeech) dervaze.Root {
 
-	ow := OttomanWord{
+	ow := dervaze.OttomanWord{
 		Visenc:           visenc,
 		Unicode:          VisencToUnicode(visenc),
 		Abjad:            VisencToAbjad(visenc),
@@ -42,22 +44,27 @@ func MakeRoot(latin string, visenc string, pos dervazepb.PartOfSpeech) dervazepb
 		DotlessSearchKey: VisencToDotlessSearchKey(visenc),
 	}
 
-	r := dervazepb.Root{
-		TurkishLatin:          latin,
-		Ottoman:               ow,
-		LastVowel:             LatinLastVowel(latin),
-		LastConsonant:         LatinLastConsonant(latin),
-		EffectiveLastVowel:    LatinEffectiveLastVowel(latin),
-		EffectiveTurkishLatin: LatinEffectiveTurkishLatin(latin),
-		EffectiveVisenc:       LatinEffectiveVisenc,
-		Abjad:                 ow.Abjad,
-		PartOfSpeech:          pos,
-		EndsWithVowel:         LatinEndsWithVowel(latin),
-		HasSingleVowel:        LatinHasSingleVowel(latin),
-		LastVowelHard:         LatinLastVowelHard(latin),
-		LastConsonantHard:     LatinLastConsonantHard(latin),
-		HasConsonantSoftening: LatinHasConsonantSoftening(latin),
+	r := dervaze.Root{
+		TurkishLatin:       latin,
+		Ottoman:            ow,
+		LastVowel:          LastVowel(latin),
+		LastConsonant:      LastConsonant(latin),
+		EffectiveLastVowel: EffectiveLastVowel(latin),
+		Abjad:              ow.Abjad,
+		PartOfSpeech:       pos,
+		EndsWithVowel:      EndsWithVowel(latin),
+		HasSingleVowel:     HasSingleVowel(latin),
+		LastVowelHard:      LastVowelHard(latin),
+		LastConsonantHard:  LastConsonantHard(latin),
+		// following three will be updated
+		EffectiveTurkishLatin: latin,
+		EffectiveVisenc:       ow.Visenc,
+		HasConsonantSoftening: false,
 	}
+
+	UpdateEffectiveSoftening(r)
+
+	return r
 }
 
 func EndsWithVowel(s string) bool {
@@ -96,7 +103,7 @@ func LastConsonant(s string) string {
 	return ultimateConsonantRegex.FindString(s)
 }
 
-func (Root *r) UpdateEffectiveSoftening() {
+func UpdateEffectiveSoftening(r *Root) {
 	// these may be modified below according to conditions
 	if !(r.hasEffectiveTurkishLatin() &&
 		r.hasEffectiveVisenc() &&
